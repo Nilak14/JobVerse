450600f08f32ea3a4f145fefba2e7ec3816058ce
Nilak Pathak <nilakpathak14@gmail.com>
2024-10-30 12:20:10 +0545

    midd

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,14 @@
-import authConfig from "./auth.config";
 import NextAuth from "next-auth";
+import authConfig from "./auth.config";
 import {
+  adminRoutePrefix,
   apiAuthPrefix,
   authRoutes,
-  DEFAULT_LOGIN_REDIRECT,
+  companyRoutePrefix,
+  DEFAULT_LOGIN_REDIRECT_ADMIN,
+  DEFAULT_LOGIN_REDIRECT_COMPANY,
+  DEFAULT_LOGIN_REDIRECT_JOB_SEEKER,
+  jobSeekerRoutePrefix,
   publicApiRoute,
   publicRoutes,
 } from "./routes";
@@ -17,33 +22,45 @@ export default auth((req) => {
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
   const isPublicApiRoute = publicApiRoute.includes(nextUrl.pathname);
+  const isCompanyRoute = nextUrl.pathname.startsWith(companyRoutePrefix);
+  const isJobSeekerRoute = nextUrl.pathname.startsWith(jobSeekerRoutePrefix);
+  const isAdminRoute = nextUrl.pathname.startsWith(adminRoutePrefix);
 
   if (isApiAuthRoute) {
     return;
   }
+
   if (isPublicApiRoute) {
     return;
   }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+      return Response.redirect(new URL("/redirect", nextUrl));
     }
     return;
   }
-
   if (!isLoggedIn && !isPublicRoute) {
     return Response.redirect(new URL("/login", nextUrl));
   }
   if (isLoggedIn && isPublicRoute) {
-    return Response.redirect(new URL("/home", nextUrl));
+    return Response.redirect(new URL("/redirect", nextUrl));
   }
-
-  return;
+  if (isLoggedIn) {
+    if (req.auth?.user.type === "JOB_SEEKER" && !isJobSeekerRoute) {
+      return Response.redirect(
+        new URL(DEFAULT_LOGIN_REDIRECT_JOB_SEEKER, nextUrl)
+      );
+    } else if (req.auth?.user.type === "COMPANY" && !isCompanyRoute) {
+      return Response.redirect(
+        new URL(DEFAULT_LOGIN_REDIRECT_COMPANY, nextUrl)
+      );
+    } else if (req.auth?.user.type === "ADMIN" && !isAdminRoute) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT_ADMIN, nextUrl));
+    }
+  }
 });
-
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
