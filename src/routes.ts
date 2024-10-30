9409883d0afea3abb0f7882109d52f3e596d0184
Nilak Pathak <nilakpathak14@gmail.com>
2024-10-30 11:03:45 +0545

    middlewreeeeeeeeeee

--- src/routes.ts
+++ src/routes.ts
@@ -12,3 +12,4 @@ export const publicApiRoute = ["/api/uploadthing", "/api/clear-uploads"];
 export const DEFAULT_LOGIN_REDIRECT_JOB_SEEKER = "/job-seeker/home";
 export const DEFAULT_LOGIN_REDIRECT_COMPANY = "/company/home";
 export const DEFAULT_LOGIN_REDIRECT_ADMIN = "/admin/dashboard";
+export const DEFAULT_LOGIN_REDIRECT = "/admin/dashboard";
