f2d024c3a5c3ca16b707970598616a55a09d3c5c
Nilak Pathak <nilakpathak14@gmail.com>
2024-10-31 19:11:09 +0545

    added loader

--- src/app/(protected)/choose/ChooseCard.tsx
+++ src/app/(protected)/choose/ChooseCard.tsx
@@ -1,20 +1,36 @@
 "use client";
 
 import { MagicCard } from "@/components/ui/magic-card";
+import { cn } from "@/lib/utils";
 import { UserType } from "@prisma/client";
-import { Building, User } from "lucide-react";
+import { Building, Loader2, User } from "lucide-react";
+import { useState } from "react";
 interface ChooseCardProps {
   updateUserType: (type: UserType) => Promise<void>;
 }
 const ChooseCard = ({ updateUserType }: ChooseCardProps) => {
+  const [loading, setLoading] = useState(false);
   return (
     <>
-      <div className="flex flex-col sm:flex-row items-center justify-between gap-4 sm:gap-0  pt-5">
+      <div
+        className={cn(
+          "flex flex-col sm:flex-row items-center justify-between gap-4 sm:gap-0 pt-5",
+          loading && "pointer-events-none"
+        )}
+      >
         <div
+          className="relative overflow-hidden "
           onClick={async () => {
+            setLoading(true);
             await updateUserType("JOB_SEEKER");
+            setLoading(false);
           }}
         >
+          {loading && (
+            <div className="bg-black/50 flex items-center justify-center rounded-md absolute inset-0 z-20">
+              <Loader2 size={30} className="animate-spin" />
+            </div>
+          )}
           <MagicCard
             gradientColor="gray"
             className=" cursor-pointer w-52 sm:w-56 h-28 sm:h-40 bg-background rounded-md border-input border-2 text-white flex justify-center flex-col gap-3 px-4"
@@ -36,10 +52,18 @@ const ChooseCard = ({ updateUserType }: ChooseCardProps) => {
           </MagicCard>
         </div>
         <div
+          className="relative z-10"
           onClick={async () => {
+            setLoading(true);
             await updateUserType("COMPANY");
+            setLoading(false);
           }}
         >
+          {loading && (
+            <div className="bg-black/50 flex items-center justify-center rounded-md absolute inset-0 z-20">
+              <Loader2 size={30} className="animate-spin" />
+            </div>
+          )}
           <MagicCard
             gradientColor="gray"
             className=" cursor-pointer w-52 sm:w-56 h-28 sm:h-40 bg-background rounded-md border-input border-2 text-white flex justify-center flex-col gap-3 px-4"
