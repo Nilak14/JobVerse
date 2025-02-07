3426b1aef7f0fb21cfd1c144328e2fd5130e120c
Nilak Pathak <nilakpathak14@gmail.com>
2025-02-07 10:12:56 +0545

    updated migrationd and database update

--- prisma/migrations/20250204194517_resume_required/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `resumeRequired` column on the `jobs` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "jobs" DROP COLUMN "resumeRequired",
-ADD COLUMN     "resumeRequired" BOOLEAN NOT NULL DEFAULT false;
