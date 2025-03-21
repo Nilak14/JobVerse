885e06e86f48cf944f810c9c9cd3324098c9898e
Nilak Pathak <nilakpathak14@gmail.com>
2025-03-21 16:58:04 +0545

    fix middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-export const publicRoutes = ["/"];
+export const publicRoutes = ["/", "/job"];
 
 export const authRoutes = [
   "/login",
@@ -20,7 +20,11 @@ export const publicApiRoute = [
   "/api/clear-uploads",
   "/api/stripe-webhook",
 ];
-export const publicContentRoute = ["/job/description", "/resume/preview"];
+export const publicContentRoute = [
+  "/job/description",
+  "/resume/preview",
+  "/job",
+];
 export const publicSEORoute = ["/sitemap.xml", "/robots.txt", "/skills.json"];
 
 export const DEFAULT_LOGIN_REDIRECT_JOB_SEEKER = "/job-seeker/dashboard";
