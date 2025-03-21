4ea4c6c979a287bc7b626fd51328ca363407e4d8
Nilak Pathak <nilakpathak14@gmail.com>
2025-03-21 09:52:30 +0545

    fix deployment issue

--- src/actions/job/saveJob.ts
+++ src/actions/job/saveJob.ts
@@ -20,6 +20,10 @@ export const saveJob = async (values: JobSchemaType) => {
     throw new Error("Unauthorized");
   }
 
+  const subscriptionLevel = await getCompanySubscriptionLevel(
+    session.activeCompanyId
+  );
+
   const {
     id: jobId,
     salaryType,
@@ -31,10 +35,6 @@ export const saveJob = async (values: JobSchemaType) => {
     ...jobValues
   } = jobSchema.parse(values);
 
-  const subscriptionLevel = await getCompanySubscriptionLevel(
-    session.activeCompanyId
-  );
-
   if (!jobId) {
     const jobCount = await prisma.job.count({
       where: { companyId: session.activeCompanyId },
@@ -45,103 +45,103 @@ export const saveJob = async (values: JobSchemaType) => {
         "You have reached the maximum number of jobs allowed for your subscription level. Please purchase a higher subscription to post more jobs."
       );
     }
+  }
 
-    const existingJob = jobId
-      ? await prisma.job.findUnique({
-          where: { id: jobId, companyId: session.activeCompanyId },
-          include: {
-            Salary: true,
+  const existingJob = jobId
+    ? await prisma.job.findUnique({
+        where: { id: jobId, companyId: session.activeCompanyId },
+        include: {
+          Salary: true,
+        },
+      })
+    : null;
+
+  if (jobId && !existingJob) {
+    throw new Error("Job not found");
+  }
+
+  if (jobId) {
+    //update the job
+    const job = await prisma.job.update({
+      where: { id: jobId },
+      data: {
+        title: values.title,
+        jobType: values.jobType,
+        workMode: values.workMode,
+        location: values.location,
+        categoryId: values.categoryId || null,
+        subcategoryId: values.subCategoryId || null,
+        experienceLevel: values.experienceLevel,
+        totalHeads: values.totalHeads,
+        benefits: values.benefits || [""],
+        description: values.description,
+        tags: values.tags,
+        skills: values.skills,
+        minEducationRequired: values.educationLevel,
+        preferredGender: values.preferredGender,
+        licenseRequired: values.license,
+        vehicleRequired: values.vehicle,
+        resumeRequired: values.resumeRequired,
+        isUrgent: values.isUrgent,
+        deadline: values.applicationDeadline,
+        sendEmailNotification: values.getEmailNotification,
+
+        Salary: {
+          delete: {
+            id: existingJob?.Salary?.id,
           },
-        })
-      : null;
-
-    if (jobId && !existingJob) {
-      throw new Error("Job not found");
-    }
-
-    if (jobId) {
-      //update the job
-      const job = await prisma.job.update({
-        where: { id: jobId },
-        data: {
-          title: values.title,
-          jobType: values.jobType,
-          workMode: values.workMode,
-          location: values.location,
-          categoryId: values.categoryId || null,
-          subcategoryId: values.subCategoryId || null,
-          experienceLevel: values.experienceLevel,
-          totalHeads: values.totalHeads,
-          benefits: values.benefits || [""],
-          description: values.description,
-          tags: values.tags,
-          skills: values.skills,
-          minEducationRequired: values.educationLevel,
-          preferredGender: values.preferredGender,
-          licenseRequired: values.license,
-          vehicleRequired: values.vehicle,
-          resumeRequired: values.resumeRequired,
-          isUrgent: values.isUrgent,
-          deadline: values.applicationDeadline,
-          sendEmailNotification: values.getEmailNotification,
-
-          Salary: {
-            delete: {
-              id: existingJob?.Salary?.id,
-            },
-            create: {
-              type: salaryType,
-              minAmount: Number(minSalaryAmount) || null,
-              maxAmount: Number(maxSalaryAmount) || null,
-              amount: Number(amount) || null,
-              currency: salaryCurrency,
-              rate: salaryRate,
-            },
+          create: {
+            type: salaryType,
+            minAmount: Number(minSalaryAmount) || null,
+            maxAmount: Number(maxSalaryAmount) || null,
+            amount: Number(amount) || null,
+            currency: salaryCurrency,
+            rate: salaryRate,
           },
         },
-      });
-      revalidatePath("/employer/job");
-      return job;
-    } else {
-      // make new entry
-      const job = await prisma.job.create({
-        data: {
-          title: values.title,
-          jobType: values.jobType,
-          workMode: values.workMode,
-          location: values.location,
-          categoryId: values.categoryId || null,
-          subcategoryId: values.subCategoryId || null,
-          experienceLevel: values.experienceLevel,
-          totalHeads: values.totalHeads,
-          benefits: values.benefits ?? [],
-          description: values.description,
-          tags: values.tags || [],
-          skills: values.skills || [],
-          minEducationRequired: values.educationLevel,
-          preferredGender: values.preferredGender,
-          licenseRequired: values.license,
-          vehicleRequired: values.vehicle,
-          resumeRequired: values.resumeRequired,
-          isUrgent: values.isUrgent,
-          deadline: values.applicationDeadline || null,
-          sendEmailNotification: values.getEmailNotification,
-          createdBy: session.employerId,
-          companyId: session.activeCompanyId,
-          Salary: {
-            create: {
-              type: salaryType,
-              minAmount: Number(minSalaryAmount),
-              maxAmount: Number(maxSalaryAmount),
-              amount: Number(amount),
-              currency: salaryCurrency,
-              rate: salaryRate,
-            },
+      },
+    });
+    revalidatePath("/employer/job");
+    return job;
+  } else {
+    // make new entry
+    const job = await prisma.job.create({
+      data: {
+        title: values.title,
+        jobType: values.jobType,
+        workMode: values.workMode,
+        location: values.location,
+        categoryId: values.categoryId || null,
+        subcategoryId: values.subCategoryId || null,
+        experienceLevel: values.experienceLevel,
+        totalHeads: values.totalHeads,
+        benefits: values.benefits ?? [],
+        description: values.description,
+        tags: values.tags || [],
+        skills: values.skills || [],
+        minEducationRequired: values.educationLevel,
+        preferredGender: values.preferredGender,
+        licenseRequired: values.license,
+        vehicleRequired: values.vehicle,
+        resumeRequired: values.resumeRequired,
+        isUrgent: values.isUrgent,
+        deadline: values.applicationDeadline || null,
+        sendEmailNotification: values.getEmailNotification,
+        createdBy: session.employerId,
+        companyId: session.activeCompanyId,
+        Salary: {
+          create: {
+            type: salaryType,
+            minAmount: Number(minSalaryAmount),
+            maxAmount: Number(maxSalaryAmount),
+            amount: Number(amount),
+            currency: salaryCurrency,
+            rate: salaryRate,
           },
         },
-      });
-      revalidatePath("/employer/job");
-      return job;
-    }
+      },
+    });
+    revalidatePath("/employer/job");
+    return job;
   }
 };
