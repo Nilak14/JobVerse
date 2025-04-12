7421c72cde4a0017d5e79f88c9558aec704d0e66
Nilak Pathak <nilakpathak14@gmail.com>
2025-04-12 23:40:07 +0545

    utils bug fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -85,6 +85,8 @@ export function mapToJobValues(data: JobServerData): JobSchemaType {
 
   return {
     id: data.id,
+    postInLinkedin: data.postInLinkedIn || false,
+    linkedinCaption: data.linkedInCaption || "",
     title: data.title || "",
     jobType: data.jobType || "",
     workMode: data.workMode || "",
